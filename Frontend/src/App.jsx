10c157c0bf860c5b9e2cf00979e3a8f3c0b5fa86
Mahnoor-Liaquat <mahnoorliaquat6@gmail.com>
2024-08-11 08:38:50 +0500

    few changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,27 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Auth/Register";
-import { Toaster } from "react-hot-toast";
-import AdminRoute from "./components/Routes/AdminRoutes";
+import { Toaster } from 'react-hot-toast'
+import AdminRoute from './components/Routes/AdminRoutes';;
 import AdminDashboard from "./pages/Admin/AdminDashboard";
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
 import Login from "./pages/Auth/Login";
 import ForgotPassword from "./pages/Auth/Forgotpassword";
 import CreateTeacher from "./pages/Admin/CreateTeacher";
 import "./pages/Admin/Admin.css";
 import CreateStudent from "./pages/Admin/CreateStudent";
-import AllStudents from "./pages/Admin/AllStudent";
+import AllStudents from './pages/Admin/AllStudent';
 import StudentDetails from "./pages/StudentDetails";
-import Teachers from "./pages/Teachers";
-import Search from "./pages/Search";
-import UpdateStudent from "./pages/Admin/UpdateStudent";
+import Teachers from './pages/Teachers';
+import Search from './pages/Search';
+import UpdateStudent from './pages/Admin/UpdateStudent';
 import TeacherStudents from "./pages/TeacherStudents";
 import StudentFilter from "./pages/Admin/StudentFilter";
 import Courses from "./pages/User/Courses";
-import Profile from "./pages/User/Profile";
 import UserDashboard from "./pages/User/UserDashboard";
 import PrivateRoute from "./components/Routes/UserRoutes";
+import PaymentsTable from "./pages/User/PaymentTable";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -36,20 +36,27 @@ function App() {
   return (
     !loading && (
       <>
-        <Toaster position="top-center" reverseOrder={false} />
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+        />
         <Routes>
-          <Route path="/home-page" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/student/:slug" element={<StudentDetails />} />
           <Route path="/students/:slug" element={<StudentDetails />} />
           <Route path="/teachers" element={<Teachers />} />
           <Route path="/search" element={<Search />} />
           <Route path="/teacher/:slug" element={<TeacherStudents />} />
 
+
           <Route path="/dashboard" element={<PrivateRoute />}>
             <Route path="user" element={<UserDashboard />} />
             <Route path="user/courses" element={<Courses />} />
-            <Route path="user/profile" element={<Profile />} />
+            <Route path="user/payment-method" element={<PaymentsTable />} />
           </Route>
           <Route path="/teacher/:slug" element={<TeacherStudents />} />
+
+
 
           <Route path="/dashboard" element={<AdminRoute />}>
             <Route path="admin" element={<AdminDashboard />} />
@@ -61,12 +68,13 @@ function App() {
           </Route>
 
           <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
         </Routes>
+
       </>
     )
-  );
+  )
 }
 
 export default App;
