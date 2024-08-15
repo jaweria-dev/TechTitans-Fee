fd5dcf869cabe1b676515217c8f541629b450ee6
jaweria-dev <jaweriaahmed25@gmail.com>
2024-08-15 09:41:41 -0700

    api added

--- Frontend/src/components/Layout/Footer.jsx
+++ Frontend/src/components/Layout/Footer.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <h1 className='text-center'>All rights reserved &copy; tect titans </h1>
+    <div className="footer">
+      <h1 className="text-center fs-6">
+        All rights reserved &copy; 2024 SMIT <span>Tech</span>{" "}
+        <span className="footer-span">Titans</span>{" "}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
